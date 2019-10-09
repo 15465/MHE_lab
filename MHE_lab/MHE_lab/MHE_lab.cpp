381c8b7c558f257ce4197845cc31961bc61ae26a
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2019-10-09 22:09:13 +0200

    lab_02

---
+++ MHE_lab/MHE_lab/MHE_lab.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+
+#define size 5
+int distance_matrix[size][size];
+int flows_matrix[size][size];
+
+float quality(int flow, int ideal_flow) {
+	return flow / ideal_flow;
+}
+
+int calculate_ideal() {
+	return NULL;
+}
+
+int main()
+{
+
+
+}
